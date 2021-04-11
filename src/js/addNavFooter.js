14d0ace1cd77a4869dbb1628b19343c135e9da5e
HeLinChooi <helinchooi@gmail.com>
2021-04-11 13:38:26 +0800

    Add footer into addNavFooter.js

--- src/js/addNavFooter.js
+++ src/js/addNavFooter.js
@@ -39,40 +39,74 @@ nav.innerHTML = `
 
 const footer = document.createElement('footer');
 footer.setAttribute('class', 'container-fluid');
-footer.innerHTML = `<div class='row d-flex justify-content-evenly bg-dark text-break text-dark py-2'>
+footer.innerHTML = `<div class='row d-flex justify-content-evenly justify-content-md-center bg-dark text-break text-dark py-2'>
 <div class='d-flex flex-column col-12 col-md-3 text-white'>
-  <button class='text-warning' type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">Contact Us</button>
-  <div class="collapse" id="collapseExample">
-
-  <span class='pt-2'>+603 7967 6300</span>
-  <span class='pt-2'>dekan_fsktm@um.edu.my</span>
-  <span class='pt-2'>FCSIT Technical Team
-  Level 2 Block A
-  Faculty of Computer Science and 
-  Information Technology
-  University of Malaya
-  50603 Kuala Lumpur</span>
+  <div>
+    <a class="btn text-white" role="button" data-toggle="collapse" data-target="#contactUs" aria-expanded="false"
+      aria-controls="contactUs"><u>Contact Us</u></a>
   </div>
+  <div class="collapse" id="contactUs">
+    <div class="d-flex flex-column px-3">
+      <span class='pt-2'>+603 7967 6300</span>
+      <span class='pt-2'>dekan_fsktm@um.edu.my</span>
+      <span class='pt-2'>FCSIT Technical Team
+        Level 2 Block A
+        Faculty of Computer Science and
+        Information Technology
+        University of Malaya
+        50603 Kuala Lumpur</span>
+    </div>
   </div>
+</div>
 <div class='d-flex flex-column col-12 col-md-3 mt-2 mt-md-0 text-white' id='links'>
-  <span class='text-warning'>Quick Links</span>
-  <a class="nostyle" href="#">Home</a>
-  <a class="nostyle" href="#">Alumni</a>
-  <a class="nostyle" href="#">Jobs</a>
-  <a class="nostyle" href="#">Events</a>
+  <div>
+    <a class="btn text-white" role="button" data-toggle="collapse" data-target="#quickLinks"
+      aria-expanded="false" aria-controls="quickLinks"><u>
+        Quick Links
+      </u>
+    </a>
+  </div>
+  <div class="collapse" id="quickLinks">
+    <div class="d-flex flex-column px-3">
+      <a class="nostyle" href="#">Home</a>
+      <a class="nostyle" href="#">Alumni</a>
+      <a class="nostyle" href="#">Jobs</a>
+      <a class="nostyle" href="#">Events</a>
+    </div>
+  </div>
 </div>
 <div class='d-flex flex-column col-12 col-md-3 mt-2 mt-md-0 text-white'>
-  <span class='text-warning'>Resources</span>
-  <a class="nostyle" href="#">UM Library</a>
-  <a class="nostyle" href="#">UM Expert</a>
-  <a class="nostyle" href="#">UM Research</a>
+  <div>
+    <a class="btn text-white" role="button" data-toggle="collapse" data-target="#resources" aria-expanded="false"
+      aria-controls="resources"><u>
+        Resources
+      </u>
+    </a>
+  </div>
+  <div class="collapse" id="resources">
+    <div class="d-flex flex-column px-3">
+      <a class="nostyle" href="#">UM Library</a>
+      <a class="nostyle" href="#">UM Expert</a>
+      <a class="nostyle" href="#">UM Research</a>
+    </div>
+  </div>
 </div>
 <div class='d-flex flex-column col-12 col-md-3 mt-2 mt-md-0 text-white'>
-  <span class='text-warning'>Follow Us</span>
-  <a class="nostyle" href="#">Instagram</a>
-  <a class="nostyle" href="#">Facebook</a>
-  <a class="nostyle" href="#">Youtube</a>
-  <a class="nostyle" href="#">Twitter</a>
+  <div>
+    <a class="btn text-white" role="button" data-toggle="collapse" data-target="#followUs" aria-expanded="false"
+      aria-controls="followUs"><u>
+        Follow Us
+      </u>
+    </a>
+  </div>
+  <div class="collapse" id="followUs">
+    <div class="d-flex flex-column px-3">
+      <a class="nostyle" href="#">Instagram</a>
+      <a class="nostyle" href="#">Facebook</a>
+      <a class="nostyle" href="#">Youtube</a>
+      <a class="nostyle" href="#">Twitter</a>
+    </div>
+  </div>
 </div>
 </div>`;
 
