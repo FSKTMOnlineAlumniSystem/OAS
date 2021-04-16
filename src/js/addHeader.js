8a2142b01dcd62e58fe54d8e6a546fee67a3d479
HeLinChooi <helinchooi@gmail.com>
2021-04-17 00:20:58 +0800

    Finalise Nav bar and footer

--- src/js/addHeader.js
+++ src/js/addHeader.js
@@ -1,21 +1,39 @@
 const body = document.body;
 const mainBody = document.getElementById('main-body');
 
-// const header = document.createElement('header');
-// header.setAttribute('class', 'd-flex flex-row-reverse align-items-center custom-dark-purple p-2 fw-bold text-white header--gradient');
-// header.append('Dr Tey Kok Soon');
-// const profileUrl = '..\\..\\..\\Assets\\imgs\\DrTey.jpg';
-// const image = document.createElement('img');
-// image.src = profileUrl;
-// image.className = `header__img m-1`;
-// const bellUrl = '..\\..\\..\\Assets\\icons\\bell.svg';
-// const image2 = document.createElement('img');
-// image2.src = bellUrl;
-// image2.className = `header__img m-1`;
-// header.appendChild(image);
-// header.appendChild(image2);
+const header = document.createElement('header');
+header.setAttribute('class', 'd-flex flex-row-reverse align-items-center header--gradient p-2 fw-bold text-white');
+header.innerHTML = `Dr Tey Kok Soon
+<img src="/Assets/imgs/DrTey.jpg" alt="" class="header__img m-1">
+<img src="/Assets/icons/bell.svg" alt="" class="header__img m-1" id='notification-icon'> 
+<div id='notification-panel'>
+  <div class="p-2 fw-bold h6 m-0">Notifications</div>
+  <ul class='m-0 list-unstyled'>
+    <li class="p-2 border-top notification-border">
+      <div class="d-flex container-fluid">
+        <div class="row">
+          <div class='col-2 d-flex justify-content-center align-items-center'>
+            <i class="fa fa-calendar fa-2x text-primary"></i>
+          </div>
+          <div class='col-8 flex-grow-1'>
+            You have been invited to join our event!! 
+            <strong>
+              Keselamatan Siber Di Kalangan Kanak-Kanak
+            </strong>
+            <div class="text-primary">Just now</div>
+          </div>
+          <div class="col-1">
+            <div class='row flex-column'>
+              <i class="fa fa-times fa-2x p-1" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+  </ul>
+</div>`
 
-// body.insertBefore(header, mainBody);
+body.insertBefore(header, mainBody);
 
 function togglePanel() {
   var notificationPanel = document.getElementById("notification-panel");
@@ -25,8 +43,7 @@ function togglePanel() {
     notificationPanel.style.display = "none";
   }
 }
-
-console.log('hi');
+togglePanel();
 // toggle notifcation bar
 const notificationPanel = document.getElementById('notification-panel');
 const notificationIcon = document.getElementById('notification-icon');
--- src/js/addNavFooter.js
+++ src/js/addNavFooter.js
@@ -4,48 +4,48 @@
 const nav = document.createElement('nav');
 nav.setAttribute('class', 'navbar navbar-light navbar-expand-md bg-light container-fluid');
 nav.innerHTML = `
-<a class="navbar-brand" href="https://fsktm.um.edu.my/" target="_blank">
-  <img src="..\\..\\..\\Assets\\imgs\\umfsktm.png" alt="" class='nav__img'>
-</a>
-<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-  <span class="navbar-toggler-icon"></span>
-</button>
-
-<div class="collapse navbar-collapse" id="navbarSupportedContent">
-  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-    <li class="nav-item">
-      <a class="nav-link active" aria-current="page" href="#">Home</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Alumni</a>
-    </li>
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-        Jobs
-      </a>
-      <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-        <li><a class="dropdown-item" href="#">All Job Advertisement</a></li>
-        <li><a class="dropdown-item" href="#">Your Job Advertisement</a></li>
-      </ul>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Event</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Contact Us</a>
-    </li>
-  </ul>
-</div>`;
+<div class="container-fluid">
+        <a class="navbar-brand" href="https://fsktm.um.edu.my/" target="_blank">
+          <img src="/Assets/imgs/umfsktm.png" alt="" class='nav__img'>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/src/html/Alumni/HomePage.html">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/src/html/Alumni/AlumniPage.html">Alumni</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Jobs
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="/src/html/Alumni/JobPage.html">All Job Advertisement</a></li>
+                <li><a class="dropdown-item" href="/src/html/Alumni/MyJobPage.html">Your Job Advertisement</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/src/html/Alumni/EventPage.html">Event</a>
+            </li>
+            <li class="nav-item">
+              <span class="nav-link" id="contact-us-nav-item">Contact Us</span>
+            </li>
+          </ul>
+        </div>
+      </div>`;
 
 const footer = document.createElement('footer');
 footer.setAttribute('class', 'container-fluid');
-footer.innerHTML = `<div class='row d-flex justify-content-evenly justify-content-md-center bg-dark text-break text-dark py-2'>
+footer.innerHTML = `<div class='row d-flex justify-content-evenly justify-content-md-center footer--gradient text-break text-dark py-2'>
 <div class='d-flex flex-column col-12 col-md-3 text-white'>
-  <div>
-    <a class="btn text-white" role="button" data-toggle="collapse" data-target="#contactUs" aria-expanded="false"
-      aria-controls="contactUs"><u>Contact Us</u></a>
+  <div class='fw-bold p-3'>
+    Contact Us
   </div>
-  <div class="collapse" id="contactUs">
+  <div id="contactUs">
     <div class="d-flex flex-column px-3">
       <span class='pt-2'>+603 7967 6300</span>
       <span class='pt-2'>dekan_fsktm@um.edu.my</span>
@@ -59,14 +59,10 @@ footer.innerHTML = `<div class='row d-flex justify-content-evenly justify-conten
   </div>
 </div>
 <div class='d-flex flex-column col-12 col-md-3 mt-2 mt-md-0 text-white' id='links'>
-  <div>
-    <a class="btn text-white" role="button" data-toggle="collapse" data-target="#quickLinks"
-      aria-expanded="false" aria-controls="quickLinks"><u>
+  <div class='fw-bold p-3'>
         Quick Links
-      </u>
-    </a>
   </div>
-  <div class="collapse" id="quickLinks">
+  <div id="quickLinks">
     <div class="d-flex flex-column px-3">
       <a class="nostyle" href="#">Home</a>
       <a class="nostyle" href="#">Alumni</a>
@@ -76,14 +72,10 @@ footer.innerHTML = `<div class='row d-flex justify-content-evenly justify-conten
   </div>
 </div>
 <div class='d-flex flex-column col-12 col-md-3 mt-2 mt-md-0 text-white'>
-  <div>
-    <a class="btn text-white" role="button" data-toggle="collapse" data-target="#resources" aria-expanded="false"
-      aria-controls="resources"><u>
+  <div class='fw-bold p-3'>
         Resources
-      </u>
-    </a>
   </div>
-  <div class="collapse" id="resources">
+  <div id="resources">
     <div class="d-flex flex-column px-3">
       <a class="nostyle" href="#">UM Library</a>
       <a class="nostyle" href="#">UM Expert</a>
@@ -92,14 +84,10 @@ footer.innerHTML = `<div class='row d-flex justify-content-evenly justify-conten
   </div>
 </div>
 <div class='d-flex flex-column col-12 col-md-3 mt-2 mt-md-0 text-white'>
-  <div>
-    <a class="btn text-white" role="button" data-toggle="collapse" data-target="#followUs" aria-expanded="false"
-      aria-controls="followUs"><u>
+  <div class='fw-bold p-3'>
         Follow Us
-      </u>
-    </a>
   </div>
-  <div class="collapse" id="followUs">
+  <div id="followUs">
     <div class="d-flex flex-column px-3">
       <a class="nostyle" href="#">Instagram</a>
       <a class="nostyle" href="#">Facebook</a>
@@ -110,5 +98,13 @@ footer.innerHTML = `<div class='row d-flex justify-content-evenly justify-conten
 </div>
 </div>`;
 
+// scroll to bottom when user click 'Contact Us'
+const contactUsNavItem = nav.querySelector('#contact-us-nav-item');
+contactUsNavItem.addEventListener('click', function(event){
+  console.log('clicked');
+  footer.scrollIntoView();
+});
+
+console.log(nav);
 body.insertBefore(nav, mainBody);
 body.appendChild(footer);
