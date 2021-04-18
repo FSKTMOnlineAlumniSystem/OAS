0ef8b0f3a1baaedf22b2d3dfc32a1dbee2acb5df
yanwen-hb <tanyanwen2@gmail.com>
2021-04-19 01:50:43 +0800

    add delete function in MyJobPage

--- src/js/Alumni/MyJobPage.js
+++ src/js/Alumni/MyJobPage.js
@@ -1,22 +1,113 @@
+import dummyResponse from '../dummydata.js';
 
-// $('.close').click(function(){
-//     var $target = $(this).parents('li');
-//     $target.hide('slow', function(){ $target.remove(); });
-//   });
-// $(document).ready(function(){
-//     $('.close').click(function(){
-//             var $target = $(this).parents('card-text');
-//             $target.hide('slow', function(){ $target.remove(); });
-//           });
-// });
+let pageIndex = 0;
+
+// document.getElementById("pageIndex").innerHTML = "";
+for(let i=1; i<=(Math.ceil(dummyResponse.Job.length/9)); i++){
+    document.getElementById("pageIndex").innerHTML += 
+    `<li class="page-item"><button class="page-link">`+i+`</button></li>`;
+    console.log('page2 ' + i)
+}
+document.getElementById("pageIndex").innerHTML +=  `<li class="page-item">
+<button class="page-link" id="nextPage">Next</button></li>` ;
 
 
-$("button").click(function () {
-    $(this).parents(".card").remove();
+const loadJobList = (pageIndex) => {
+    // document.getElementById('pageIndex').innerHTML = pageIndex+1+"/"+Math.ceil(dummyResponse.Job.length/9);
+   
+    // console.log(Math.ceil(dummyResponse.Job.length/9));
+   
+    document.getElementById('jobList').innerHTML = "";
+    let jobStartIndex = pageIndex * 9;
+    let jobEndIndex = jobStartIndex + 9;
+    console.log(pageIndex)
+    console.log(jobStartIndex)
+    console.log(jobEndIndex)
+
+    if(jobEndIndex>=dummyResponse.Job.length){
+        document.getElementById("nextPage").disabled = true;
+        // console.log('here')
+    }
+    else{
+        document.getElementById("nextPage").disabled = false;
+    }
+    if(pageIndex==0){
+        document.getElementById("previousPage").disabled = true;
+        // console.log('last page')
+    }
+    else{
+        document.getElementById("previousPage").disabled = false;
+    }
+    // document.getElementById('jobList').innerHTML += '<div class="card-desk">' +'<div class="row row-cols-4">'
+    
+    for (let i = jobStartIndex; i < jobEndIndex && i < dummyResponse.Job.length; i++) {
+        document.getElementById('jobList').innerHTML += 
+        `<div class="col mb-4">
+        <div class="card h-100">
+        
+        <img src="../../../Assets/imgs/${dummyResponse.Job[i].imageId}" class="card-img-top" alt="jobPhoto">
+        <div class="card-body">
+        <h5 class="card-title">${dummyResponse.Job[i].title}</h5>
+        <p class="card-text">
+        <div class="row cards">
+        <div class="col-1"> <img src="../../../Assets/imgs/locationIcon.png" alt="location" width="30" height="30"></div>
+        <div class="col-7">${dummyResponse.Job[i].location}</div>
+        </div>
+        <div class="row cards">
+        <div class="col-1">  <img src="../../../Assets/imgs/salaryIcon.png" alt="time" height="24" width="24"></div>
+        <div class="col-7">${dummyResponse.Job[i].salary}</div>
+        </div>
+        </p>
+        </div>
+        <div class="card-footer mt-auto">
+        <button type="button" class="close" role="button" aria-pressed="true" data-name=${dummyResponse.Job[i].jobId}><i class="bi bi-trash-fill"></i></button>  
+        </div></div><div>`;
+    }
+}
+
+$("#jobList").on("click", ".close", function(event){
+    var name = $(this).attr("data-name");
+    for(let i=0; i<dummyResponse.Job.length; i++){
+        if(dummyResponse.Job[i].jobId == name){
+            dummyResponse.Job.splice(i,1);  //at position i remove 1 item
+            console.log('delete');
+            break;
+        }
+    }
+    loadJobList(pageIndex);
+    console.log(name);
 });
 
-// $("button").click(function () {
-//     $(this).closest('div[class="col-md-4 col-xs-4"]').slideUp("slow", function(){
-//       $(this).remove();
-//     });
-//   });
+function removeJob(name){
+    for(let i=0; i<dummyResponse.Job.length; i++){
+        if(dummyResponse.Job[i].jobId == name){
+            dummyResponse.Job.splice(i,1);  //at position i remove 1 item
+            console.log('delete');
+            break;
+        }
+    }
+    
+};
+
+// for (var i in cart) {
+//     if (cart[i].name === name) {
+//         cart.splice(i, 1);
+//         break;
+//     }
+// }
+// saveCart();
+
+
+document.getElementById('nextPage').addEventListener("click", function(){
+    pageIndex++;
+    loadJobList(pageIndex);
+    console.log('here' + pageIndex)
+});
+
+document.getElementById('previousPage').addEventListener("click", function(){
+    pageIndex--;
+    loadJobList(pageIndex);
+    console.log('next' + pageIndex)
+});
+
+loadJobList(pageIndex);
