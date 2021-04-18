8863c9c3ba51812c2def90e03f7e62c6da393af7
yanwen-hb <tanyanwen2@gmail.com>
2021-04-18 18:15:08 +0800

    upload jobPage js

--- src/js/Alumni/JobPage.js
+++ src/js/Alumni/JobPage.js
@@ -2,35 +2,45 @@ import dummyResponse from '../dummydata.js';
 
 let pageIndex = 0;
 
-const loadJobList = (pageIndex) => {
-    document.getElementById('pageIndex').innerHTML = pageIndex+1+"/"+Math.ceil(dummyResponse.Job.length/10);
-    console.log(pageIndex);
-    console.log(dummyResponse.Job.length)
+// document.getElementById("pageIndex").innerHTML = "";
+for(let i=1; i<=(Math.ceil(dummyResponse.Job.length/9)); i++){
+    document.getElementById("pageIndex").innerHTML += 
+    `<li class="page-item"><button class="page-link">`+i+`</button></li>`;
+    console.log('page2 ' + i)
+}
+document.getElementById("pageIndex").innerHTML +=  `<li class="page-item">
+<button class="page-link" id="nextPage">Next</button></li>` ;
 
+
+const loadJobList = (pageIndex) => {
+    // document.getElementById('pageIndex').innerHTML = pageIndex+1+"/"+Math.ceil(dummyResponse.Job.length/9);
+   
+    // console.log(Math.ceil(dummyResponse.Job.length/9));
+   
     document.getElementById('jobList').innerHTML = "";
-    let jobStartIndex = pageIndex * 3;
-    let jobEndIndex = jobStartIndex + 3;
+    let jobStartIndex = pageIndex * 9;
+    let jobEndIndex = jobStartIndex + 9;
     console.log(pageIndex)
     console.log(jobStartIndex)
     console.log(jobEndIndex)
 
     if(jobEndIndex>=dummyResponse.Job.length){
-        document.getElementById('nextPage').disabled = true;
+        document.getElementById("nextPage").disabled = true;
         console.log('here')
     }
     else{
-        document.getElementById('nextPage').disabled = false;
+        document.getElementById("nextPage").disabled = false;
     }
     if(pageIndex==0){
-        document.getElementById('previousPage').disabled = true;
+        document.getElementById("previousPage").disabled = true;
         // console.log('last page')
     }
     else{
-        document.getElementById('previousPage').disabled = false;
+        document.getElementById("previousPage").disabled = false;
     }
     // document.getElementById('jobList').innerHTML += '<div class="card-desk">' +'<div class="row row-cols-4">'
-    for(let j=0; j<2 ; j++){
-    for (let i = jobStartIndex; i < jobEndIndex*4 && i < dummyResponse.Job.length; i++) {
+    
+    for (let i = jobStartIndex; i < jobEndIndex && i < dummyResponse.Job.length; i++) {
         document.getElementById('jobList').innerHTML += 
         `<div class="col mb-4">
         <div class="card h-100">
@@ -50,40 +60,30 @@ const loadJobList = (pageIndex) => {
         </div></div></div>`;
     }
 }
-}
 
 
-const nextPage = () => {
+// const nextPage = () => {
+//     pageIndex++;
+//     loadJobList(pageIndex);
+// }
+
+document.getElementById('nextPage').addEventListener("click", function(){
     pageIndex++;
     loadJobList(pageIndex);
-}
+    console.log('here' + pageIndex)
+});
 
-const previousPage = () => {
+
+// const previousPage = () => {
+//     pageIndex--;
+//     loadJobList(pageIndex);
+// }
+
+document.getElementById('previousPage').addEventListener("click", function(){
     pageIndex--;
     loadJobList(pageIndex);
-}
+    console.log('next' + pageIndex)
+});
 
 loadJobList(pageIndex);
 
-// document.getElementById('jobList').innerHTML += `<div class="card-desk"> <div class="row row-cols-4">` 
-
-// for (let i = 0; i < dummyResponse.Job.length; i++) {
-//     document.getElementById('jobList').innerHTML += 
-//     `<div class="col mb-4">
-//     <div class="card h-100">
-//     <img src="../../../Assets/imgs/${dummyResponse.Job[i].imageId}" class="card-img-top" alt="jobPhoto">
-//     <div class="card-body">
-//     <h5 class="card-title">${dummyResponse.Job[i].title}</h5>
-//     <p class="card-text">
-//     <div class="row cards">
-//     <div class="col-1"> <img src="../../../Assets/imgs/locationIcon.png" alt="location" width="30" height="30"></div>
-//     <div class="col-7">${dummyResponse.Job[i].location}</div>
-//     </div>
-//     <div class="row cards">
-//     <div class="col-1">  <img src="../../../Assets/imgs/salaryIcon.png" alt="time" height="24" width="24"></div>
-//     <div class="col-7">${dummyResponse.Job[i].salary}</div>
-//     </div>
-//     </p>
-//     </div></div></div>`;
-// }
-// document.getElementById('jobList').innerHTML += `</div> </div>`；
