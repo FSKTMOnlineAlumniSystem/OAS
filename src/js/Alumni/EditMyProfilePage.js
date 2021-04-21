755ff590635bcd88614aeaadfc30a0bb46e8f08b
Yong Ming <yongming1124@gmail.com>
2021-04-22 01:04:07 +0800

    Revert "Admin-MyProfilePage & Admin-EditMyProfilePage done"
    This reverts commit bafc94b80cfbf2416032f50e5a2ba87a8a1db1a9.

--- src/js/Alumni/EditMyProfilePage.js
+++ src/js/Alumni/EditMyProfilePage.js
@@ -14,6 +14,7 @@ const closeCancelChangesModalButton = document.querySelector('#closeCancelChange
 const stayButton = document.querySelector('#stayButton');
 const choosePictureDescription = document.querySelector('#choosePictureDescription');
 
+
 const currentAlumniId = "AL-1";
 const alumni = dummyResponse.Alumni.filter(function (alumni) {
     return alumni.alumniId === currentAlumniId;
@@ -34,7 +35,6 @@ function setValid(el) {
     }
 }
 
-/*Check the file extension of the image & Update preview*/
 img.addEventListener('change', (e) => readURL(e));
 function readURL(e) {
     let allowedExtensions =
