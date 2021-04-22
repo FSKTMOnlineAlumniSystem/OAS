f4c5db8ecda009fdf9d73ff6350a1bcb2f7325e5
CWH <ahaoahao2000@gmail.com>
2021-04-23 01:43:51 +0800

    update alumni page
    redesign the button
    implement dummy response

--- src/js/Alumni/alumniPage.js
+++ src/js/Alumni/alumniPage.js
@@ -1,138 +1,105 @@
-console.log('testing')
-const dummyData = {
-    alumniList: [
-        {
-            imgPath: "../../../Assets/imgs/DrTey.jpg",
-            alumniName: "Dr XXX",
-            description1: "Bachelor of Computer Science (Hons), graduated 2010",
-            description2: "XXX is currently working at HTM Niseko, Hokkaido in Japan, building and expanding systems as a full stack lead developer."
-        
-        },
-        {
-            imgPath: "/Assets/imgs/tey.jpg",
-            alumniName: "Dr Teh",
-            description1: "Bachelor of Computer Science (Hons), graduated 2010",
-            description2: "XXX is currently working at HTM Niseko, Hokkaido in Japan, building and expanding systems as a full stack lead developer."
-        },
-        {
-            imgPath: "tey.jpg",
-            alumniName: "Dr XXX",
-            description1: "Bachelor of Computer Science (Hons), graduated 2010",
-            description2: "XXX is currently working at HTM Niseko, Hokkaido in Japan, building and expanding systems as a full stack lead developer."
-        },
-        {
-            imgPath: "tey.jpg",
-            alumniName: "Dr Teh",
-            description1: "Bachelor of Computer Science (Hons), graduated 2010",
-            description2: "XXX is currently working at HTM Niseko, Hokkaido in Japan, building and expanding systems as a full stack lead developer."
-        },
-        {
-            imgPath: "tey.jpg",
-            alumniName: "Dr XXX",
-            description1: "Bachelor of Computer Science (Hons), graduated 2010",
-            description2: "XXX is currently working at HTM Niseko, Hokkaido in Japan, building and expanding systems as a full stack lead developer."
-        },
-        {
-            imgPath: "tey.jpg",
-            alumniName: "Dr Teh",
-            description1: "Bachelor of Computer Science (Hons), graduated 2010",
-            description2: "XXX is currently working at HTM Niseko, Hokkaido in Japan, building and expanding systems as a full stack lead developer."
-        },
-        {
-            imgPath: "tey.jpg",
-            alumniName: "Dr XXX",
-            description1: "Bachelor of Computer Science (Hons), graduated 2010",
-            description2: "XXX is currently working at HTM Niseko, Hokkaido in Japan, building and expanding systems as a full stack lead developer."
-        },
-        {
-            imgPath: "tey.jpg",
-            alumniName: "Dr Teh",
-            description1: "Bachelor of Computer Science (Hons), graduated 2010",
-            description2: "XXX is currently working at HTM Niseko, Hokkaido in Japan, building and expanding systems as a full stack lead developer."
-        },
-        {
-            imgPath: "tey.jpg",
-            alumniName: "Dr XXX",
-            description1: "Bachelor of Computer Science (Hons), graduated 2010",
-            description2: "XXX is currently working at HTM Niseko, Hokkaido in Japan, building and expanding systems as a full stack lead developer."
-        },
-        {
-            imgPath: "tey.jpg",
-            alumniName: "Dr Teh",
-            description1: "Bachelor of Computer Science (Hons), graduated 2010",
-            description2: "XXX is currently working at HTM Niseko, Hokkaido in Japan, building and expanding systems as a full stack lead developer."
-        },
-        {
-            imgPath: "tey.jpg",
-            alumniName: "Dr Ang1",
-            description1: "Bachelor of Computer Science (Hons), graduated 2000",
-            description2: "ANG1 is currently working at UM"
-        },
-        {
-            imgPath: "tey.jpg",
-            alumniName: "Dr Ang2",
-            description1: "Bachelor of Computer Science (Hons), graduated 2000",
-            description2: "ANG2 is currently working at UKM"
-        },
-    ]
-}
-
+import dummyResponse from "../dummydata.js";
+console.log("link js");
 let pageIndex = 0;
-
 const loadAlumniList = (pageIndex) => {
-    document.getElementById('pageIndex').innerHTML = pageIndex+1+"/"+Math.ceil(dummyData.alumniList.length/10);
-    document.getElementById('alumniList').innerHTML = "";
-    let alumniStartIndex = pageIndex * 10;
-    let alumniEndIndex = alumniStartIndex + 10;
-    console.log(pageIndex)
-    // console.log('alumniEndIndex'+alumniEndIndex)
-    // console.log('length'+dummyData.alumniList.length)
-    if(alumniEndIndex>=dummyData.alumniList.length){
-        document.getElementById('btn nextPage-btn btn-sm').disabled = true;
-        console.log('last page')
-    }
-    else{
-        document.getElementById('btn nextPage-btn btn-sm').disabled = false;
-    }
-    if(pageIndex==0){
-        document.getElementById('btn previousPage-btn btn-light btn-sm').disabled = true;
-        console.log('last page')
-    }
-    else{
-        document.getElementById('btn previousPage-btn btn-light btn-sm').disabled = false;
-    }
-    for (let i = alumniStartIndex; i < alumniEndIndex && i < dummyData.alumniList.length; i++) {
-        document.getElementById('alumniList').innerHTML += 
-        `<div class="media justify-content-center mb-2 w-75" style="background-color:#E9E5E5;">
+  let alumniStartIndex = pageIndex * 10;
+  let alumniEndIndex = alumniStartIndex + 10;
+
+  var dataLength = dummyResponse.Alumni.length;
+  var remainingLength = dataLength - alumniStartIndex;
+  /*   js for button*/
+  if (alumniEndIndex >= dummyResponse.Alumni.length) {
+    document.getElementById("nextPage").innerHTML = `
+        <li class="page-item disabled">
+        <button id="nextPage"  onclick="nextPage()" class="page-link" tabindex="-1" aria-disabled="true">Next</button>
+      </li>`;
+    console.log("last page");
+  } else {
+    document.getElementById("nextPage").innerHTML = `
+        <li class="page-item" id="nextPage">
+            <button  onclick="nextPage()" class="page-link" >Next</button>
+          </li>`;
+  }
+  if (pageIndex == 0) {
+    document.getElementById("previousPage").innerHTML = `
+        <li class="page-item disabled">
+        <button id="previousPage"  onclick="previousPage()" class="page-link" tabindex="-1" aria-disabled="true">Previous</button>
+      </li>`;
+    console.log("first page");
+  } else {
+    document.getElementById("previousPage").innerHTML = `
+        <li class="page-item" id="previousPage">
+            <button onclick="previousPage()" class="page-link">Previous</button>
+          </li>`;
+  }
+  // js for 1,2,3
+  if (remainingLength <= 10) {
+    console.log("<=10");
+    document.getElementsByClassName("pages")[0].innerHTML = `
+            <li class="page-item disabled">
+            <button class="btn btn-link page-item" style="text-decoration: none" tabindex="-1" aria-disabled="true">${
+              pageIndex + 1
+            }</button>
+            </li>`;
+  } else if (remainingLength <= 20) {
+    console.log("<=20");
+    document.getElementsByClassName("pages")[0].innerHTML = `
+            <li class="page-item disabled">
+            <button class="btn btn-link page-item" style="text-decoration: none" tabindex="-1" aria-disabled="true">${
+              pageIndex + 1
+            }</button>
+            </li>
+            <li class="page-item" ><button class="btn btn-link" style="text-decoration: none" onclick="nextPage()">${
+              pageIndex + 2
+            }</button></li>`;
+  } else {
+    console.log("<=30");
+    document.getElementsByClassName("pages")[0].innerHTML = `
+            <li class="page-item disabled">
+            <button class="btn btn-link page-item" style="text-decoration: none" tabindex="-1" aria-disabled="true">${
+              pageIndex + 1
+            }</button>
+            </li>
+            <li class="page-item" ><button class="btn btn-link" style="text-decoration: none" onclick="nextPage()">${
+              pageIndex + 2
+            }</button ></li>
+            <li class="page-item" ><button class="btn btn-link" style="text-decoration: none" onclick="nextPage();nextPage()">${
+              pageIndex + 3
+            }</button></li>`;
+  }
+
+  for (
+    let i = alumniStartIndex;
+    i < alumniEndIndex && i < dummyResponse.Alumni.length;
+    i++
+  ) {
+    document.getElementById(
+      "alumniList"
+    ).innerHTML += `<div class="media justify-content-center mb-2 w-75" style="background-color:#E9E5E5;">
         <div class="image ml-5 mt-2">
-        <img src=${dummyData.alumniList[i].imgPath} class="mr-3" alt=${dummyData.alumniList[i].alumniName} height=auto width="80">
+        <img src=${dummyResponse.Alumni[i].imageId} class="mr-3" alt=${dummyResponse.Alumni[i].name} height=auto width="80">
         </div>
         <div class="media-body mr-3">
-        <h6 class="mt-0">${dummyData.alumniList[i].alumniName}</h6>
-        <p>${dummyData.alumniList[i].description1}</p>
+        <h6 class="mt-0">${dummyResponse.Alumni[i].name}</h6>
+        <p>${dummyResponse.Alumni[i].department}</p>
         <p style="display: -webkit-box;
         -webkit-line-clamp: 3;
         -webkit-box-orient: vertical;
         overflow: hidden;
-        text-overflow: ellipsis;">${dummyData.alumniList[i].description2}</p>
+        text-overflow: ellipsis;">${dummyResponse.Alumni[i].biography}</p>
         </div></div>
         </div>`;
-    }
-}
+  }
+};
 
-/* <span class="d-block">
-${dummyData.alumniList[i].alumniName}
-</span></div> */
-const nextPage = () => {
-    pageIndex++;
-    loadAlumniList(pageIndex);
-}
+window.nextPage = function () {
+  pageIndex++;
+  loadAlumniList(pageIndex);
+};
 
-const previousPage = () => {
-    pageIndex--;
-    loadAlumniList(pageIndex);
-}
+window.previousPage = function () {
+  pageIndex--;
+  loadAlumniList(pageIndex);
+};
 
 loadAlumniList(pageIndex);
-
-
