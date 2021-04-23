dfcf7eab8c630eedb6227e1808daac1e18586b62
Yong Ming <yongming1124@gmail.com>
2021-04-23 20:35:26 +0800

    copy redesign from backup

--- src/js/Alumni/alumniPage.js
+++ src/js/Alumni/alumniPage.js
@@ -1,7 +1,10 @@
 import dummyResponse from "../dummydata.js";
-console.log("link js");
+
+const alumniList = document.getElementById("alumniList");
+
 let pageIndex = 0;
 const loadAlumniList = (pageIndex) => {
+  alumniList.innerHTML = "";
   let alumniStartIndex = pageIndex * 10;
   let alumniEndIndex = alumniStartIndex + 10;
 
@@ -37,35 +40,29 @@ const loadAlumniList = (pageIndex) => {
     console.log("<=10");
     document.getElementsByClassName("pages")[0].innerHTML = `
             <li class="page-item disabled">
-            <button class="btn btn-link page-item" style="text-decoration: none" tabindex="-1" aria-disabled="true">${
-              pageIndex + 1
-            }</button>
+            <button class="btn btn-link page-item" style="text-decoration: none" tabindex="-1" aria-disabled="true">${pageIndex + 1
+      }</button>
             </li>`;
   } else if (remainingLength <= 20) {
     console.log("<=20");
     document.getElementsByClassName("pages")[0].innerHTML = `
             <li class="page-item disabled">
-            <button class="btn btn-link page-item" style="text-decoration: none" tabindex="-1" aria-disabled="true">${
-              pageIndex + 1
-            }</button>
+            <button class="btn btn-link page-item" style="text-decoration: none" tabindex="-1" aria-disabled="true">${pageIndex + 1
+      }</button>
             </li>
-            <li class="page-item" ><button class="btn btn-link" style="text-decoration: none" onclick="nextPage()">${
-              pageIndex + 2
-            }</button></li>`;
+            <li class="page-item" ><button class="btn btn-link" style="text-decoration: none" onclick="nextPage()">${pageIndex + 2
+      }</button></li>`;
   } else {
     console.log("<=30");
     document.getElementsByClassName("pages")[0].innerHTML = `
             <li class="page-item disabled">
-            <button class="btn btn-link page-item" style="text-decoration: none" tabindex="-1" aria-disabled="true">${
-              pageIndex + 1
-            }</button>
+            <button class="btn btn-link page-item" style="text-decoration: none" tabindex="-1" aria-disabled="true">${pageIndex + 1
+      }</button>
             </li>
-            <li class="page-item" ><button class="btn btn-link" style="text-decoration: none" onclick="nextPage()">${
-              pageIndex + 2
-            }</button ></li>
-            <li class="page-item" ><button class="btn btn-link" style="text-decoration: none" onclick="nextPage();nextPage()">${
-              pageIndex + 3
-            }</button></li>`;
+            <li class="page-item" ><button class="btn btn-link" style="text-decoration: none" onclick="nextPage()">${pageIndex + 2
+      }</button ></li>
+            <li class="page-item" ><button class="btn btn-link" style="text-decoration: none" onclick="nextPage();nextPage()">${pageIndex + 3
+      }</button></li>`;
   }
 
   for (
@@ -73,22 +70,22 @@ const loadAlumniList = (pageIndex) => {
     i < alumniEndIndex && i < dummyResponse.Alumni.length;
     i++
   ) {
-    document.getElementById(
-      "alumniList"
-    ).innerHTML += `<div class="media justify-content-center mb-2 w-75" style="background-color:#E9E5E5;">
-        <div class="image ml-5 mt-2">
-        <img src=${dummyResponse.Alumni[i].imageId} class="mr-3" alt=${dummyResponse.Alumni[i].name} height=auto width="80">
-        </div>
-        <div class="media-body mr-3">
-        <h6 class="mt-0">${dummyResponse.Alumni[i].name}</h6>
-        <p>${dummyResponse.Alumni[i].department}</p>
-        <p style="display: -webkit-box;
-        -webkit-line-clamp: 3;
-        -webkit-box-orient: vertical;
-        overflow: hidden;
-        text-overflow: ellipsis;">${dummyResponse.Alumni[i].biography}</p>
-        </div></div>
-        </div>`;
+    alumniList.innerHTML += `<div class="media justify-content-center mb-2 w-75 p-3" style="background-color:#E9E5E5;">
+    <div class="image m-auto col-2 p-3">
+    <div style="aspect-ratio:1/1;overflow:hidden;">
+    <img src=${dummyResponse.Alumni[i].imageId} class="w-100" alt=${dummyResponse.Alumni[i].name}>
+    </div>
+    </div>
+    <div class="media-body mr-3 my-auto col-10">
+    <h6 class="mt-0 mb-1">${dummyResponse.Alumni[i].name}</h6>
+    <em class="mb-0">Bachelor of Computer Science (${dummyResponse.Alumni[i].department}), graduated ${dummyResponse.Alumni[i].graduated}</em>
+    <small style="display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;">${dummyResponse.Alumni[i].biography}</small>
+    </div></div>
+    </div>`;
   }
 };
 
