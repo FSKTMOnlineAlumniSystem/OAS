e1cafe933f232a4fad0da358ea494fff45c9d47a
nic-00 <55591460+nic-00@users.noreply.github.com>
2021-04-24 13:43:04 +0800

    Login Page version1

---
+++ src/js/Alumni/LoginPage.js
@@ -0,0 +1,224 @@
+// function openSignUp(){
+//     document.getElementById('signUP').style.display='block';
+// }
+
+// function closeSignUp(){
+//     window.onclick = function(event) {
+//         if (event.target == popUp) {
+//           popUp.style.display = "none";
+//         }
+//       }
+// }
+
+// function openSignIn(){
+//     document.getElementById('signIN').style.display='block';
+//     window.onclick = function(event) {
+//         if (event.target == popUp) {
+//           popUp.style.display = "none";
+//         }
+//       }
+// }
+
+const form_2 = document.getElementById('forgot');
+const sendEmail = document.getElementById('sendEmail');
+
+
+const form_1 = document.getElementById('signIN');
+const staticEmail = document.getElementById('staticEmail');
+const inputPassword = document.getElementById('inputPassword');
+
+
+const form = document.getElementById('signUP');
+const FirstName = document.getElementById('FirstNameID');
+const LastName = document.getElementById('LastNameID');
+const Email = document.getElementById('Email');
+const IC = document.getElementById('IC');
+const password = document.getElementById('Password');
+const Department = document.getElementById('Department');
+const Batch = document.getElementById('Batch');
+const Gender = document.getElementById('Gender');
+
+
+
+const emailFormat = /[a-zA-Z0-9]+@[a-z0-9]+(\.[a-z]+)+/;
+
+form_2.addEventListener('submit', (evt) => {
+
+    let errorExist = false;
+
+    const sendEmailValue = sendEmail.value.trim();
+
+    if (isEmpty(sendEmailValue) || !sendEmail.value.match(emailFormat)) {
+        setErrorFor(sendEmail);
+        errorExist = true;
+    } else {
+        setSuccessFor(sendEmail);
+    }
+
+    if (errorExist) {
+        evt.preventDefault();}
+
+});
+
+
+form_1.addEventListener('submit', (ev) => {
+
+    let errorExist = false;
+
+    const staticEmailValue = staticEmail.value.trim();
+    const inputPasswordValue = inputPassword.value.trim();
+
+    console.log(staticEmailValue);
+    console.log(inputPasswordValue);
+
+
+    if (isEmpty(staticEmailValue) || !staticEmail.value.match(emailFormat)) {
+        setErrorFor(staticEmail);
+        errorExist = true;
+    } else {
+        setSuccessFor(staticEmail);
+    }
+
+
+    if(isEmpty(inputPasswordValue)) {
+        setErrorFor(inputPassword);
+        errorExist = true;
+    }else{
+        setSuccessFor(inputPassword);
+    }
+
+
+    if (errorExist) {
+        ev.preventDefault();}
+
+});
+
+
+
+form.addEventListener('submit', (e) => {
+
+    let errorExist = false;
+
+    const FirstNameValue = FirstNameID.value.trim();
+    const emailValue = Email.value.trim();
+    const LastNameValue = LastNameID.value.trim();
+    const ICValue = IC.value.trim();
+    const passwordValue = Password.value.trim();
+    const DepartmentValue = Department.value.trim();
+    const BatchValue = Batch.value.trim();
+    const GenderValue = Gender.value.trim();
+
+    
+
+
+    if(DepartmentValue == 0){
+        console.log("errordep");
+        setErrorFor(Department);
+        errorExist = true;
+        }else{
+            console.log("succdep");
+            setSuccessFor(Department);
+        }
+
+    if(BatchValue == 0){
+        console.log("errordep");
+        setErrorFor(Batch);
+        errorExist = true;
+        }else{
+            console.log("succdep");
+            setSuccessFor(Batch);
+        }
+
+    if(GenderValue == 0){
+        console.log("errordep");
+        setErrorFor(Gender);
+        errorExist = true;
+        }else{
+            console.log("succdep");
+            setSuccessFor(Gender);
+        }
+
+    if(isEmpty(FirstNameValue)){
+        setErrorFor(FirstName);
+        errorExist = true;
+    }else{
+        setSuccessFor(FirstName);
+    }
+
+    if(isEmpty(LastNameValue)){
+        setErrorFor(LastName);
+        errorExist = true;
+    }else{
+        setSuccessFor(LastName);
+    }
+
+
+    if (isEmpty(emailValue) || !Email.value.match(emailFormat)) {
+        setErrorFor(Email);
+        errorExist = true;
+    } else {
+        setSuccessFor(Email);
+    }
+
+
+    if(isEmpty(ICValue) || (ICValue.length == 11)){
+        setErrorFor(IC);
+        errorExist = true;
+    }else{
+        setSuccessFor(IC);
+    }
+
+
+    if(isEmpty(passwordValue) || checkLength(passwordValue)) {
+        setErrorFor(Password);
+        errorExist = true;
+    }else{
+        setSuccessFor(Password);
+    }
+
+
+    if (errorExist) {
+        e.preventDefault();}
+
+    
+});
+
+
+
+ function checkLength(passwordValue) {
+     console.log('checkmethod');
+    if(Password.value.length < 6 ){
+        return true;
+    }else if(Password.value.length > 20 ){
+        return true;
+    }else{
+        return false;
+    }
+     
+ }
+
+function setErrorFor(input) {
+    if (input.classList.contains("is-valid")) {
+        input.classList.replace("is-valid", "is-invalid");
+    } else {
+        input.classList.add("is-invalid");
+    }
+}
+
+function setSuccessFor(input) {
+    if (input.classList.contains("is-invalid")) {
+        input.classList.replace("is-invalid", "is-valid");
+    } else {
+        input.classList.add("is-valid");
+    }
+}
+
+//  function isEmail(email) {
+//      console.log("isemail");
+//     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+//  }
+
+ function isEmpty(obj) {
+    return obj.length == 0;
+}
+ 
