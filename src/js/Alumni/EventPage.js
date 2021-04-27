2a434f5ee03377d9192a1c07c9401159e5c6879f
HeLinChooi <helinchooi@gmail.com>
2021-04-27 12:34:26 +0800

    Add addsearchBar.js & 2 icons
    The eventPage.js modified to make design better.
    The design involve the number of card per row when the breakpoint is lg

--- src/js/Alumni/EventPage.js
+++ src/js/Alumni/EventPage.js
@@ -9,8 +9,29 @@ const myEvent = dummyResponse.Alumni_Event.filter(event => {
 const notMyEvent = dummyResponse.Alumni_Event.filter(event => {
   return event.alumniId !== localStorage.getItem('SignedInAlumniId');
 });
-console.log(notMyEvent);
-
+// <img src="/Assets/imgs/calendar.png" alt="time" height="24" width="24">
+// <img src="/Assets/imgs/time.png" alt="time" height="24" width="24">
+const getCardStructure = (eventId, imageId, title, dateTime) => {
+  return `<a href="/src/html/Alumni/EventDetailsPage.html" target="_self" id="${eventId}-card">
+  <div class="card h-100">
+    <img src="/Assets/imgs/${imageId}" class="card-img-top image__fixed-height" alt="eventPhoto">
+    <div class="card-body">
+      <h5 class="card-title">${title}</h5>
+      <p class="card-text">
+      <div class="row cards">
+        <div class="col-1"><i class="far fa-calendar-alt text-danger"></i>
+        </div>
+        <div class="col-7">${getReadableDate(dateTime)}</div>
+      </div>
+      <div class="row cards">
+        <div class="col-1"><i class="far fa-clock text-primary"></i></div>
+        <div class="col-9">${getReadableTime(dateTime)}</div>
+      </div>
+      </p>
+    </div>
+  </div>
+</a>`
+}
 // build html nodes based on myEvent
 myEvent.forEach(event => {
   const eventDetails = dummyResponse.Event.filter(evt => evt.eventId === event.eventId)[0];
@@ -19,32 +40,11 @@ myEvent.forEach(event => {
   
   // /src/html/Alumni/EventDetailsPage.html
   const cardDiv = document.createElement('div');
-  cardDiv.setAttribute('class', 'col-12 col-sm-6 col-md-4 mb-4');
-  cardDiv.innerHTML = `
-  <a href="/src/html/Alumni/EventDetailsPage.html" target="_self" id="${event.eventId}-card">
-    <div class="card h-100">
-      <img src="/Assets/imgs/${imageId}" class="card-img-top image__fixed-height" alt="eventPhoto">
-      <div class="card-body">
-        <h5 class="card-title">${title}</h5>
-        <p class="card-text">
-        <div class="row cards">
-          <div class="col-1"><img src="/Assets/imgs/calendar.png" alt="time" height="24" width="24">
-          </div>
-          <div class="col-7">${getReadableDate(dateTime)}</div>
-        </div>
-        <div class="row cards">
-          <div class="col-1"> <img src="/Assets/imgs/time.png" alt="time" height="24" width="24"></div>
-          <div class="col-9">${getReadableTime(dateTime)}</div>
-        </div>
-        </p>
-      </div>
-    </div>
-  </a>`;
+  cardDiv.setAttribute('class', 'col-12 col-sm-6 col-md-4 col-lg-3 mb-4');
+  cardDiv.innerHTML = getCardStructure(event.eventId, imageId, title, dateTime);
   yourUpcomingEventSection.appendChild(cardDiv);
 
   // function to be called when this card clicked
-  // console.log(yourUpcomingEventSection.lastChild.querySelector('a'));
-  // console.log(document.getElementById(event.eventId + '-card'));
   // console.log(cardDiv.querySelector('#' + event.eventId + '-card'));
   const evtHandler = evt => {
     console.log(`set eventId to ${event.eventId}`);
@@ -58,27 +58,8 @@ notMyEvent.forEach(event => {
   const { title, dateTime, imageId } = eventDetails;
   
   const cardDiv = document.createElement('div');
-  cardDiv.setAttribute('class', 'col-12 col-sm-6 col-md-4 mb-4');
-  cardDiv.innerHTML = `
-  <a href="/src/html/Alumni/EventDetailsPage.html" target="_self" id="${event.eventId}-card">
-    <div class="card h-100">
-      <img src="/Assets/imgs/${imageId}" class="card-img-top image__fixed-height" alt="eventPhoto">
-      <div class="card-body">
-        <h5 class="card-title">${title}</h5>
-        <p class="card-text">
-        <div class="row cards">
-          <div class="col-1"><img src="/Assets/imgs/calendar.png" alt="time" height="24" width="24">
-          </div>
-          <div class="col-7">${getReadableDate(dateTime)}</div>
-        </div>
-        <div class="row cards">
-          <div class="col-1"> <img src="/Assets/imgs/time.png" alt="time" height="24" width="24"></div>
-          <div class="col-9">${getReadableTime(dateTime)}</div>
-        </div>
-        </p>
-      </div>
-    </div>
-  </a>`;
+  cardDiv.setAttribute('class', 'col-12 col-sm-6 col-md-4 col-lg-3 mb-4');
+  cardDiv.innerHTML = getCardStructure(event.eventId, imageId, title, dateTime);
   upcomingEventSection.appendChild(cardDiv);
 
   // function to be called when this card clicked
