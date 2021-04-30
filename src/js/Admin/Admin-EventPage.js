8df4f7f1079094bb8560c7332eb1819cc98815a5
Yong Ming <yongming1124@gmail.com>
2021-04-30 22:51:36 +0800

    solve bugs

--- src/js/Admin/Admin-EventPage.js
+++ src/js/Admin/Admin-EventPage.js
@@ -1,4 +1,4 @@
-import {dummyResponse, updateDummyData} from "../dummydata.js";
+import { dummyResponse, updateDummyData } from "../dummydata.js";
 const imgPath = "/Assets/imgs/";
 
 
@@ -7,7 +7,7 @@ let pageIndex = 0;
 const loadEventList = (pageIndex) => {
   let eventStartIndex = pageIndex * 10;
   let eventEndIndex = eventStartIndex + 10;
- 
+
   var dataLength = dummyResponse.Event.length;
   var remainingLength = dataLength - eventStartIndex;
   /*   js for button*/
@@ -16,7 +16,7 @@ const loadEventList = (pageIndex) => {
         <li class="page-item disabled">
         <button id="nextPage"  onclick="nextPage()" class="page-link" tabindex="-1" aria-disabled="true">Next</button>
       </li>`;
-   } else {
+  } else {
     document.getElementById("nextPage").innerHTML = `
         <li class="page-item" id="nextPage">
             <button  onclick="nextPage()" class="page-link" >Next</button>
@@ -27,7 +27,7 @@ const loadEventList = (pageIndex) => {
         <li class="page-item disabled">
         <button id="previousPage"  onclick="previousPage()" class="page-link" tabindex="-1" aria-disabled="true">Previous</button>
       </li>`;
-    } else {
+  } else {
     document.getElementById("previousPage").innerHTML = `
         <li class="page-item" id="previousPage">
             <button   onclick="previousPage()" class="page-link">Previous</button>
@@ -35,7 +35,7 @@ const loadEventList = (pageIndex) => {
   }
   // js for 1,2,3
   if (remainingLength <= 10) {
-     document.getElementsByClassName("pages")[0].innerHTML = `
+    document.getElementsByClassName("pages")[0].innerHTML = `
         <li class="page-item disabled">
         <button class="page-link" tabindex="-1" aria-disabled="true">${pageIndex + 1
       }</button>
@@ -88,7 +88,7 @@ const loadEventList = (pageIndex) => {
                  <div style="font-weight: 200; font-size: 14px">${getReadableTime(dummyResponse.Event[i].dateTime)}</div>
                
                  <td style="font-weight: 400; font-size: 18px" class="eventTitle">
-                 <a class="eventTitle" id=${i} href="Admin-EventPageUpdate.html" data-toggle="modal" data-target="#titleModal">
+                 <a class="eventTitle" id=${i} data-toggle="modal" data-target="#titleModal">
                 ${dummyResponse.Event[i].title
       }
               </a>
@@ -125,6 +125,18 @@ const loadEventList = (pageIndex) => {
 
     newRow.innerHTML = newRowContent;
   }
+
+  var clickedAlumniIndex;
+  document.querySelectorAll('.eventTitle').forEach((title) => {
+    title.addEventListener('click', (e) => {
+      clickedAlumniIndex = e.target.id;
+      document.querySelector('#imageTitle').src = imgPath + dummyResponse.Event[clickedAlumniIndex].imageId;
+      document.querySelector('#title').textContent = dummyResponse.Event[clickedAlumniIndex].title;
+      document.querySelector('#description').textContent = dummyResponse.Event[clickedAlumniIndex].description;
+      document.querySelector('#location').textContent = dummyResponse.Event[clickedAlumniIndex].location;
+      localStorage.setItem('updateId', clickedAlumniIndex);
+    });
+  })
 };
 
 window.toggle = function (source) {
@@ -154,19 +166,12 @@ window.previousPage = function () {
 };
 loadEventList(pageIndex);
 
-// window.onload = function (i) {
-//   console.log("function is running")
-//   // localStorage.setItem("eventID",dummyResponse.Event[i].eventId)
-//   // console.log("event id"+ dummyResponse.Event[i].eventId)
-//   console.log(i)
-//   // console.log(dummyResponse.Event[i].eventId)
-// }
 
 
 window.updateEvent = function (o) {
   var eventId = o.id.split(" ")[1]
   localStorage.setItem("updateId", eventId)
-  location.href="Admin-EventPageUpdate.html"
+  location.href = "Admin-EventPageUpdate.html"
 }
 
 window.DeleteRowFunction = function (o) {
@@ -180,9 +185,9 @@ window.DeleteRowFunction = function (o) {
 }
 window.DeleteCheckedRow = function () {
   var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-  for (var i = checkboxes.length-1; i > 0; i--) {
-    if(checkboxes[i].checked){
-      dummyResponse.Event.splice(i-1, 1)
+  for (var i = checkboxes.length - 1; i > 0; i--) {
+    if (checkboxes[i].checked) {
+      dummyResponse.Event.splice(i - 1, 1)
     }
   }
   checkboxes[0].checked = false;
@@ -191,36 +196,24 @@ window.DeleteCheckedRow = function () {
   // location.reload();
 }
 
-var clickedAlumniId;
-document.querySelectorAll('.eventTitle').forEach((title) => {
-  title.addEventListener('click',(e)=>{
-		clickedAlumniId = e.target.id;
-    // localStorage.setItem('updateId',clickedAlumniId);
-    console.log("hiiiiiii"+clickedAlumniId)
-		document.querySelector('#title').textContent = dummyResponse.Event[clickedAlumniId].title
-    document.querySelector('#description').textContent = dummyResponse.Event[clickedAlumniId].description
-    document.querySelector('#location').textContent = dummyResponse.Event[clickedAlumniId].location
-    
 
-  })
-})
 
 // var updateId;
 // document.querySelector('#editButton').addEventListener {
-  
-  
+
+
 // }
 
 // document.querySelectorAll().forEach(btn=>
 //   {
 //     btn.addEventListener('click',(e)=>{
-//       clickedAlumniId = e.target.id;
+//       clickedAlumniIndex = e.target.id;
 //     })
 //   }
 //   )
 
 // filter
-var searchBar=document.getElementById('searchBar');
+var searchBar = document.getElementById('searchBar');
 searchBar.addEventListener('click', (e) => {
   e.preventDefault();
   var input, filter, table, tr, td, i;
@@ -231,62 +224,62 @@ searchBar.addEventListener('click', (e) => {
   for (var i = 1; i < tr.length; i++) {
     var tds = tr[i].getElementsByTagName("td");
     var flag = false;
-    for(var j = 0; j < tds.length; j++){
+    for (var j = 0; j < tds.length; j++) {
       var td = tds[j];
       if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
         flag = true;
-      } 
+      }
     }
-    if(flag){
-        tr[i].style.display = "";
+    if (flag) {
+      tr[i].style.display = "";
     }
     else {
-        tr[i].style.display = "none";
+      tr[i].style.display = "none";
     }
   }
 });
 
 $(document).ready(function () {
-    $("#status,#department").on("change", function () {
-        var status = $('#status').find("option:selected").val();
-        var department = $('#department').find("option:selected").val();
-        SearchData(status, department)
-    });
+  $("#status,#department").on("change", function () {
+    var status = $('#status').find("option:selected").val();
+    var department = $('#department').find("option:selected").val();
+    SearchData(status, department)
+  });
 });
 
-window.SearchData = function(status, department) {
-    if (status.toUpperCase() == 'ALL' && department.toUpperCase() == 'ALL') {
-        $('#myTable tbody tr').show();
-    } else {
-        $('#myTable tbody tr:has(td)').each(function () {
-            var rowStatus = $.trim($(this).find('td:eq(4)').text());
-            var rowDepartment = $.trim($(this).find('td:eq(3)').text());
-            if (status.toUpperCase() != 'ALL' && department.toUpperCase() != 'ALL') {
-                if (rowStatus.toUpperCase() == status.toUpperCase() && rowDepartment == department) {
-                    $(this).show();
-                } else {
-                    $(this).hide();
-                }
-            } else if ($(this).find('td:eq(4)').text() != '' || $(this).find('td:eq(4)').text() != '') {
-                if (status != 'All' || department == 'All') {
-                    if (rowStatus.toUpperCase() == status.toUpperCase()) {
-                        $(this).show();
-                    } else {
-                        $(this).hide();
-                    }
-                }
-                if (department != 'All' || status =='All') {
-                    if (rowDepartment == department) {
-                        $(this).show();
-                    }
-                    else {
-                        $(this).hide();
-                    }
-                }
-            }
+window.SearchData = function (status, department) {
+  if (status.toUpperCase() == 'ALL' && department.toUpperCase() == 'ALL') {
+    $('#myTable tbody tr').show();
+  } else {
+    $('#myTable tbody tr:has(td)').each(function () {
+      var rowStatus = $.trim($(this).find('td:eq(4)').text());
+      var rowDepartment = $.trim($(this).find('td:eq(3)').text());
+      if (status.toUpperCase() != 'ALL' && department.toUpperCase() != 'ALL') {
+        if (rowStatus.toUpperCase() == status.toUpperCase() && rowDepartment == department) {
+          $(this).show();
+        } else {
+          $(this).hide();
+        }
+      } else if ($(this).find('td:eq(4)').text() != '' || $(this).find('td:eq(4)').text() != '') {
+        if (status != 'All' || department == 'All') {
+          if (rowStatus.toUpperCase() == status.toUpperCase()) {
+            $(this).show();
+          } else {
+            $(this).hide();
+          }
+        }
+        if (department != 'All' || status == 'All') {
+          if (rowDepartment == department) {
+            $(this).show();
+          }
+          else {
+            $(this).hide();
+          }
+        }
+      }
 
-        });
-    }
+    });
+  }
 }
 /*Close Modal */
 closeCancelChangesModalButton.addEventListener('click', () => closeModal('#cancelChangesModal'));
@@ -294,60 +287,3 @@ stayButton.addEventListener('click', () => closeModal('#cancelChangesModal'));
 function closeModal(modalId) {
   $(modalId).modal('hide');
 }
- //model
-//  document.querySelectorAll('.eventTitle').forEach((title) => {
-//   title.addEventListener('click', (e) => {
-//     console.log(document.getElementById("exampleModal"));
-//     document.getElementById("exampleModal").innerHTML =
-//       `<div class="modal-dialog">
-//                   <div class="modal-content">
-//                     <div class="modal-header">
-//                       <h5 class="modal-title" id="exampleModalLabel">Event</h5>
-//                       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-//                         <span aria-hidden="true">&times;</span>
-//                       </button>
-//                     </div>
-//                     <div class="modal-body" >
-//                       <img src="${imgPath+dummyResponse.Event[e.target.id].imageId}" class="mx-auto d-block" alt="name" width="200px" height="auto">
-//                       <br>
-//                       <!-- <p>Schedule :${`${new Intl.DateTimeFormat("en", { day: "2-digit" }).format(new Date(dummyResponse.Event[e.target.id].dateTime))}, 
-//                       ${new Intl.DateTimeFormat("en", { month: "short" }).format(new Date(dummyResponse.Event[e.target.id].dateTime))} 
-//                       ${new Intl.DateTimeFormat("en", { year: "numeric" }).format(new Date(dummyResponse.Event[e.target.id].dateTime))}`}; 
-//                       ${new Date(dummyResponse.Event[e.target.id].dateTime).toLocaleTimeString()}</p> -->
-//                       <p> Title : ${dummyResponse.Event[e.target.id].title}</p>
-//                       <p>Description : ${dummyResponse.Event[e.target.id].description}</p>
-//                       <p>Location : ${dummyResponse.Event[e.target.id].location} </p>
-//                     </div>
-//                     <div class="modal-footer">
-//                       <button type="button" class="btn btn-primary"
-//                         onclick="location.href = 'Admin-EventPageUpdate.html'; ${localStorage.setItem('updateId', e.target.id)}">
-//                         <i class="fas fa-edit" >
-//                     </i>
-//                         Edit</button>
-//                     </div>
-//                   </div>
-//                 </div>`
-//   }
-//   )
-// }
-// )
-
-
-//
-// console.log(document.getElementById("titleModal"));
-// document.getElementById("modal-body").innerHTML =
-//   `
-//                   <img src="${imgPath+dummyResponse.Event[e.target.id].imageId}" class="mx-auto d-block" alt="name" width="200px" height="auto">
-//                   <br>
-//                   <!-- <p>Schedule :${`${new Intl.DateTimeFormat("en", { day: "2-digit" }).format(new Date(dummyResponse.Event[e.target.id].dateTime))}, 
-//                   ${new Intl.DateTimeFormat("en", { month: "short" }).format(new Date(dummyResponse.Event[e.target.id].dateTime))} 
-//                   ${new Intl.DateTimeFormat("en", { year: "numeric" }).format(new Date(dummyResponse.Event[e.target.id].dateTime))}`}; 
-//                   ${new Date(dummyResponse.Event[e.target.id].dateTime).toLocaleTimeString()}</p> -->
-//                   <p> Title : ${dummyResponse.Event[e.target.id].title}</p>
-//                   <p>Description : ${dummyResponse.Event[e.target.id].description}</p>
-//                   <p>Location : ${dummyResponse.Event[e.target.id].location} </p>
-//                 `
-//                 console.log('target id: '+e.target.id);
-//                 localStorage.setItem('updateId', e.target.id)
-//                 // localStorage.setItem('UpdateId',e.target.id);
-//                 $("#titleModal").modal()
