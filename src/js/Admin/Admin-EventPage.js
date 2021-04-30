d5f4ddd0f3853b9fc63f07ddff6a0aa2f55db4f7
Xinjoe <joyce.lxj.8a@gmail.com>
2021-04-30 20:34:44 +0800

    add comment delete unused code

--- src/js/Admin/Admin-EventPage.js
+++ src/js/Admin/Admin-EventPage.js
@@ -1,18 +1,11 @@
 import {dummyResponse, updateDummyData} from "../dummydata.js";
 const imgPath = "/Assets/imgs/";
 console.log("link js");
-// model
-// $('#myModal').on('shown.bs.modal', function () {
-//   $('#myInput').trigger('focus')
-// })
-//eventList
 let pageIndex = 0;
 
 const loadEventList = (pageIndex) => {
   console.log(dummyResponse)
-  // document.getElementById('pageIndex').innerHTML = pageIndex + 1 + "/" + Math.ceil(dummyResponse.Event.length / 10);
   console.log("the length" + dummyResponse.Event.length);
-  // document.getElementById('eventList').innerHTML = "";
   let eventStartIndex = pageIndex * 10;
   let eventEndIndex = eventStartIndex + 10;
   console.log("page index" + pageIndex);
@@ -21,6 +14,7 @@ const loadEventList = (pageIndex) => {
 
   var dataLength = dummyResponse.Event.length;
   var remainingLength = dataLength - eventStartIndex;
+
   /*   js for button*/
   if (eventEndIndex >= dummyResponse.Event.length) {
     document.getElementById("nextPage").innerHTML = `
@@ -46,6 +40,7 @@ const loadEventList = (pageIndex) => {
             <button   onclick="previousPage()" class="page-link">Previous</button>
           </li>`;
   }
+
   // js for 1,2,3
   if (remainingLength <= 10) {
     console.log("<=10");
@@ -77,7 +72,6 @@ const loadEventList = (pageIndex) => {
       }</button></li>`;
   }
 
-  // }
   var table = document.getElementsByClassName(
     "table table-striped table-sm something"
   )[0];
@@ -92,18 +86,6 @@ const loadEventList = (pageIndex) => {
     i < eventEndIndex && i < dummyResponse.Event.length;
     i++
   ) {
-    // console.log("previous time" + dummyResponse.Event[i].dateTime)
-    // var d = new Date(dummyResponse.Event[i].dateTime);
-    // let ye = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d);
-    // let mo = new Intl.DateTimeFormat("en", { month: "short" }).format(d);
-    // let da = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
-    
-    // d.toLocaleDateString('en-GB')
-
-
-    // var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    //     if (checkboxes[i] != source)
-    //         checkboxes[i].checked = source.checked;
 
     var newRowContent = `<tr class="rowss">
                 
@@ -151,12 +133,11 @@ const loadEventList = (pageIndex) => {
       .getElementsByClassName("table table-striped table-sm something")[0]
       .getElementsByTagName("tbody")[0];
     var newRow = tableRef.insertRow(tableRef.rows.length);
-    // table.insertRow(newRow)
-    // console.log(newRow)
     newRow.innerHTML = newRowContent;
   }
 };
 
+// check all 
 window.toggle = function (source) {
   console.log("checkbox")
   var checkboxes = document.querySelectorAll('input[type="checkbox"]');
@@ -166,7 +147,7 @@ window.toggle = function (source) {
   }
 }
 
-
+// check single box
 window.check = function (source, i) {
   console.log("check row")
   var checkboxes = document.querySelectorAll('input[type="checkbox"]');
@@ -184,22 +165,6 @@ window.previousPage = function () {
 };
 loadEventList(pageIndex);
 
-
-
-
-
-
-
-
-window.onload = function (i) {
-  console.log("function is running")
-  // localStorage.setItem("eventID",dummyResponse.Event[i].eventId)
-  // console.log("event id"+ dummyResponse.Event[i].eventId)
-  console.log(i)
-  // console.log(dummyResponse.Event[i].eventId)
-}
-
-
 window.updateEvent = function (o) {
   var eventId = o.id.split(" ")[1]
   localStorage.setItem("updateId", eventId)
@@ -215,12 +180,9 @@ window.DeleteRowFunction = function (o) {
   dummyResponse.Event.splice(findId, 1)
   console.log(dummyResponse.Event)
   updateDummyData(dummyResponse)
-  // var p = o.parentNode.parentNode.parentNode;
-  // console.log(p)
-  // p.parentNode.removeChild(p);
   loadEventList(pageIndex)
-  // location.reload();
 }
+
 window.DeleteCheckedRow = function () {
   var checkboxes = document.querySelectorAll('input[type="checkbox"]');
   for (var i = checkboxes.length-1; i > 0; i--) {
@@ -231,14 +193,8 @@ window.DeleteCheckedRow = function () {
   checkboxes[0].checked = false;
   updateDummyData(dummyResponse)
   loadEventList(pageIndex)
-  // location.reload();
 }
-// window.addRow = function () {
-//   console.log('add row')
-//   var table = document.getElementsByClassName(
-//     "table table-striped table-sm something"
-//   )[0];
-// }
+
 
 document.querySelectorAll('.eventTitle').forEach((title) => {
   title.addEventListener('click', (e) => {
@@ -277,30 +233,6 @@ document.querySelectorAll('.eventTitle').forEach((title) => {
 }
 )
 // filter
-// window.filterSearchBar = function() {
-//   var input, filter, table, tr, td, i;
-//   input = document.getElementById("searchBar");
-//   filter = input.value.toUpperCase();
-//   table = document.getElementsByClassName("table-responsive")[0];
-//   tr = table.getElementsByTagName("tr");
-//   for (var i = 1; i < tr.length; i++) {
-//     var tds = tr[i].getElementsByTagName("td");
-//     var flag = false;
-//     for(var j = 0; j < tds.length; j++){
-//       var td = tds[j];
-//       if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-//         flag = true;
-//       } 
-//     }
-//     if(flag){
-//         tr[i].style.display = "";
-//     }
-//     else {
-//         tr[i].style.display = "none";
-//     }
-//   }
-// }
-
 var searchBar=document.getElementById('searchBar');
 searchBar.addEventListener('click', (e) => {
   e.preventDefault();
@@ -370,112 +302,3 @@ window.SearchData = function(status, department) {
         });
     }
 }
- //model
-//  document.getElementById("schedule").innerHTML =
-
-// `Schedule :${`${new Intl.DateTimeFormat("en", { day: "2-digit" }).format(new Date(dummyResponse.Event[e.target.id].dateTime))}, 
-// ${new Intl.DateTimeFormat("en", { month: "short" }).format(new Date(dummyResponse.Event[e.target.id].dateTime))} 
-// ${new Intl.DateTimeFormat("en", { year: "numeric" }).format(new Date(dummyResponse.Event[e.target.id].dateTime))}`}; 
-// ${new Date(dummyResponse.Event[e.target.id].dateTime).toLocaleTimeString()}`;
-// document.getElementById("title").innerHTML =
-// `Title : ${dummyResponse.Event[e.target.id].title}`;
-// document.getElementById("description").innerHTML =
-// `Description : ${dummyResponse.Event[e.target.id].description}`;
-// document.getElementById("location").innerHTML =
-// `Location : ${dummyResponse.Event[e.target.id].location}`;
- //model
-//  <div class="modal fade p-5" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-//  <div class="modal-dialog">
-//    <div class="modal-content">
-//      <div class="modal-header">
-//        <h5 class="modal-title" id="exampleModalLabel">Event</h5>
-//        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-//          <span aria-hidden="true">&times;</span>
-//        </button>
-//      </div>
-//      <div class="modal-body" >
-//        <img src="/Assets/imgs/E-1.jpg" class="mx-auto d-block" alt="name" width="200px" height="auto">
-//        <br>
-//        <p>Schedule :${`${da}, ${mo} ${ye}`}; ${d.toLocaleTimeString()}</p>
-//        <p> Title : ${dummyResponse.Event[i].title}</p>
-//        <p>Description : ${dummyResponse.Event[i].description}</p>
-//        <p>Location : ${dummyResponse.Event[i].location} </p>
-//      </div>
-//      <div class="modal-footer">
-//        <button type="button" class="btn btn-success"
-//          onclick="location.href = 'Admin-EventPageUpdate.html';">Edit</button>
-//      </div>
-//    </div>
-//  </div>
-// </div>
-
-//  $('button.fa fa-trash fa-3x pl-2').on('click', function (e) {
-//   e.preventDefault();
-//   var id = $(this).closest('tr').data('id');
-//   $('#myModal').data('id', id).modal('show');
-// });
-
-// $('#btn btn-primary').click(function () {
-//   var id = $('#myModal').data('id');
-//   $('[data-id=' + id + ']').remove();
-//   $('#myModal').modal('hide');
-// });
-
-
-//model 
-/*        var newRowContent = `<tr class="rowss">
-            <td>
-                <div class="custom-control custom-checkbox mr-sm-2 ml-2">
-                    <input type="checkbox" class="custom-control-input" id="Boxes1">
-                    <label class="custom-control-label" for="Boxes1"></label>
-                </div>
-            </td>
-             <td style="font-weight: 400; font-size: 18px">${`${da}, ${mo} ${ye}`}
-             <div style="font-weight: 200; font-size: 14px">${d.toLocaleTimeString()}</div>
-            </td>
-            <td style="font-weight: 400; font-size: 18px">${
-              dummyResponse.Event[i].title
-            }</td>
-            <td style="font-weight: 400; font-size: 14px; overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical;">${
-              dummyResponse.Event[i].description
-            }</td>
-            <td style="font-weight: 200; font-size: 18px">${
-              dummyResponse.Event[i].location
-            }</td>
-            <td>
-            <div class="btn-group" role="group" aria-label="Third group">
-            <a href="#" role="button"><i class="fa fa-pencil fa-3x pr-2" aria-hidden="true"
-                style="color: rgb(0, 0, 0); font-size: 35px">
-              </i></a>
-
-              <a href="#" role="button" data-toggle="modal" data-target="#exampleModal"><i
-              class="fa fa-trash fa-3x pl-2" aria-hidden="true"
-              style="color: rgb(255, 49, 49); font-size: 35px">
-            </i></a>
-
-                    <!-- Modal -->
-                  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-                    aria-hidden="true">
-                    <div class="modal-dialog">
-                      <div class="modal-content">
-                        <div class="modal-header">
-                          <h5 class="modal-title" id="exampleModalLabel">Delete Event</h5>
-                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                          </button>
-                        </div>
-                        <div class="modal-body">
-                          Are you sure you want to delete event?
-                        </div>
-                        <div class="modal-footer">
-                          <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
-                          <button type="button" class="btn btn-primary" <!--value="Delete Row" onclick="DeleteRowFunction(this)"-->>Yes</button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-
-              </div>
-            </td>
-        </tr>`;
-        */
