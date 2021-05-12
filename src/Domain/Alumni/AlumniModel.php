8b9811ee685d4dab637e5196a0513f06848800f9
CWH <ahaoahao2000@gmail.com>
2021-05-12 23:18:04 +0800

    connect db

---
+++ src/Domain/Alumni/AlumniModel.php
@@ -0,0 +1,47 @@
+<?php
+
+class AlumniModel
+{
+  private PDO $connection;
+
+    public function __construct(PDO $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function getAll(): array
+    {
+        try {
+            $stmt = $this->connection->prepare('SELECT * FROM alumni');
+            $stmt->execute();
+            $data = $stmt->fetchAll();
+
+            if (!$data) {
+                return array();
+            }
+            return $data;
+
+        } catch (PDOException $exception) {
+            error_log('ActivityModel: getAll: ' . $exception->getMessage());
+            throw $exception;
+        }
+    }
+
+  // public function getById(int $id): array
+  // {
+
+  //   try {
+  //     $stmt = $this->connection->prepare('SELECT * FROM activity WHERE id = ?');
+  //     $stmt->execute([$id]);
+  //     $data = $stmt->fetch();
+
+  //     if (!$data) {
+  //       return array();
+  //     }
+  //     return $data;
+  //   } catch (PDOException $exception) {
+  //     error_log('ActivityModel: getById: ' . $exception->getMessage() . ' id: ' . $id);
+  //     throw $exception;
+  //   }
+  // }
+}
---
+++ src/Domain/Alumni/AlumniPage.php
@@ -0,0 +1,59 @@
+<?php
+include '../header.php';
+?>
+<link rel="stylesheet" type="text/css" href="/public/css/Alumni/AlumniPage.css" />
+
+  <title><?= $GLOBALS['title']; ?></title>
+</head>
+<body>
+  
+  <?php
+include '../../../config/config.php';
+include './AlumniModel.php';
+include '../Database.php';
+
+$db = new Database(DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD);
+
+try {
+  $event_model = new AlumniModel($db->getConnection());
+  $all_activities = $event_model->getAll();
+  if (!empty($all_activities)) {
+
+    foreach ($all_activities as $activity) {
+      echo "$activity[alumniId] ";
+    }
+  }
+} catch (Exception $e) {
+  echo $e->getMessage();
+}
+
+?>
+
+<main class="container my-5" id="main-body">
+      <h1>
+        Alumni -
+        <i
+          style="
+            font-weight: lighter;
+            font-family: 'Times New Roman', Times, serif;
+          "
+          >“ Awards become corroded. Alumni gather no dust. ”</i
+        >
+      </h1>
+      <br />
+      <div id="alumniList"></div>
+      <br />
+      <span id="pageIndex"></span>
+      <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+          <div id="previousPage"></div>
+          <div class="pages list-group list-group-horizontal"></div>
+          <div id="nextPage"></div>
+        </ul>
+      </nav>
+      <br />
+    </main>
+  <!-- custom js files -->
+  <script type="module" src="/public/js/Alumni/AlumniPage.js"></script>
+  
+  <?php include '../footer.php' ?>
