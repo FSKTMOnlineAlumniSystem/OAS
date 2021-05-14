e81c1f1662469cb7fcc7e8a1890319179d4be871
HeLinChooi <helinchooi@gmail.com>
2021-05-14 14:05:09 +0800

    Fix nav toggle bug

--- src/Domain/footer.php
+++ src/Domain/footer.php
@@ -5,7 +5,7 @@
 <script type="text/javascript" src="/public/js/addSearchBar.js"></script>
 <script type="module" src="/public/js/Alumni/searchAlgo.js"></script>
 <!-- bootstrap javascript files -->
-<script src="/libs/bootstrap/js/bootstrap.bundle.js"></script>
+<script src="../../libs/bootstrap.bundle.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 </body>
