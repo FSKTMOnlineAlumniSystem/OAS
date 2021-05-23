524dd014219a834428cd7cbc91b2f942cbbe3062
rachelong <57581517+rachelong@users.noreply.github.com>
2021-05-23 17:56:09 +0800

    Revert "Merge branch '(Admin)-Manage-Event' into (Admin)-Manage-Alumni-Account"
    This reverts commit 57c09229e9f53e2639809af77d66d4ebf94d96cf, reversing
    changes made to 909ce9663eec165f78118cc5e07850e7b5a3d985.

--- public/js/Admin/Admin-EventPageUpdate.js
+++ public/js/Admin/Admin-EventPageUpdate.js
@@ -1,7 +1,7 @@
 sessionStorage.setItem('event', 'update')
-// import {dummyResponse, updateDummyData} from "../dummydata.js";
+import {dummyResponse, updateDummyData} from "../dummydata.js";
 const imgPath = "/Assets/imgs/";
-let eventArray=event_array
+
 var inputValue;
 var imageName;
 var inputValue;
@@ -19,116 +19,112 @@ const stayButton = document.querySelector('#stayButton');
 const choosePictureDescription = document.querySelector('#choosePictureDescription');
 
 var i = localStorage.getItem("updateId")
-localStorage.setItem('eventId',eventArray[i].eventId)
-var d = new Date(eventArray[i].dateTime);
+localStorage.setItem('eventId',dummyResponse.Event[i].eventId)
+var d = new Date(dummyResponse.Event[i].dateTime);
 var todayDate = d.toISOString().slice(0, 10);
-document.getElementById('dateTime').value=d; //for php reference
+
 // const minute = d.getMinutes();
 let hour = d.getHours();
 let minute = d.getMinutes().toString();
   minute = minute.padStart(2, '0');
 
-document.getElementById('title').value=eventArray[i].title
+document.getElementById('title').value=dummyResponse.Event[i].title
 document.getElementById('date').value=todayDate
 document.getElementById('time').value=hour +':'+ minute
-document.getElementById('description').value=eventArray[i].description
-document.getElementById('location').value=eventArray[i].location
-document.getElementById('prevImage').src=imgPath+eventArray[i].imageId
+document.getElementById('description').value=dummyResponse.Event[i].description
+document.getElementById('location').value=dummyResponse.Event[i].location
+document.getElementById('prevImage').src=imgPath+dummyResponse.Event[i].imageId
 
-var titlevalue = eventArray[i].title;
-  var descriptionvalue = eventArray[i].description;
-  var locatevalue = eventArray[i].location;
+var titlevalue = dummyResponse.Event[i].title;
+  var descriptionvalue = dummyResponse.Event[i].description;
+  var locatevalue = dummyResponse.Event[i].location;
   var datevalue = todayDate;
   var timevalue = hour +':'+ minute;
 
-// function setInValid(el) {
-//   if (el.classList.contains("is-valid")) {
-//     el.classList.replace("is-valid", "is-invalid");
-//   } else {
-//     el.classList.add("is-invalid");
-//   }
-// }
-// function setValid(el) {
-//   if (el.classList.contains("is-invalid")) {
-//     el.classList.replace("is-invalid", "is-valid");
-//   } else {
-//     el.classList.add("is-valid");
-//   }
-// }
+function setInValid(el) {
+  if (el.classList.contains("is-valid")) {
+    el.classList.replace("is-valid", "is-invalid");
+  } else {
+    el.classList.add("is-invalid");
+  }
+}
+function setValid(el) {
+  if (el.classList.contains("is-invalid")) {
+    el.classList.replace("is-invalid", "is-valid");
+  } else {
+    el.classList.add("is-valid");
+  }
+}
 
 /*Check the file extension of the image & Update preview*/
-// img.addEventListener('change', (e) => readURL(e));
-// function readURL(e) {
-//   let allowedExtensions =
-//     /(\.png|\.jpg|\.jpeg)$/i;
-//   if (e.target.files && e.target.files[0] && allowedExtensions.test(e.target.value)) {
-//     var reader = new FileReader();
-//     reader.onload = function (e) {
-//       document.getElementById("prevImage").src = e.target.result;
-//     }
-//     reader.readAsDataURL(e.target.files[0]);
-//     choosePictureDescription.textContent = "";
-//   } else {
-//     choosePictureDescription.textContent = "Please choose picture in .png, .jpg or .jpeg format";
+img.addEventListener('change', (e) => readURL(e));
+function readURL(e) {
+  let allowedExtensions =
+    /(\.png|\.jpg|\.jpeg)$/i;
+  if (e.target.files && e.target.files[0] && allowedExtensions.test(e.target.value)) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      document.getElementById("prevImage").src = e.target.result;
+    }
+    reader.readAsDataURL(e.target.files[0]);
+    choosePictureDescription.textContent = "";
+  } else {
+    choosePictureDescription.textContent = "Please choose picture in .png, .jpg or .jpeg format";
     
-//   }
-// }
-
-
+  }
+}
 /*Form Validation for Edit My Profile (email, contactNumber, biography)*/
-// function isEmpty(obj) {
-//   return obj.value.length == 0;
-// }
+function isEmpty(obj) {
+  return obj.value.length == 0;
+}
 
-// form.addEventListener('submit', (e) => {
-  // e.preventDefault();
-//   function checkvalidation() {
-//     console.log("here");
-//   let errorExist = false; //false if no error exists in email, contactNumber, biography
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let errorExist = false; //false if no error exists in email, contactNumber, biography
 
-//   if (isEmpty(title)) {
-//     setInValid(title);
-//     errorExist = true;
-//   } else {
-//     setValid(title);
-//   }
+  if (isEmpty(title)) {
+    setInValid(title);
+    errorExist = true;
+  } else {
+    setValid(title);
+  }
 
-//   if (isEmpty(description)) {
-//     setInValid(description);
-//     errorExist = true;
-//   } else {
-//     setValid(description);
-//   }
+  if (isEmpty(description)) {
+    setInValid(description);
+    errorExist = true;
+  } else {
+    setValid(description);
+  }
 
-//   if (isEmpty(locate)) {
-//     setInValid(locate);
-//     errorExist = true;
-//   } else {
-//     setValid(locate);
-//   }
+  if (isEmpty(locate)) {
+    setInValid(locate);
+    errorExist = true;
+  } else {
+    setValid(locate);
+  }
 
-//   if (isEmpty(date)) {
-//     setInValid(date);
-//     errorExist = true;
-//   } else {
-//     setValid(date);
-//   }
+  if (isEmpty(date)) {
+    setInValid(date);
+    errorExist = true;
+  } else {
+    setValid(date);
+  }
 
-//   if (isEmpty(time)) {
-//     setInValid(time);
-//     errorExist = true;
-//   } else {
-//     setValid(time);
-//   }
+  if (isEmpty(time)) {
+    setInValid(time);
+    errorExist = true;
+  } else {
+    setValid(time);
+  }
 
-//   if (!errorExist) {
-//     update_array();
-//     saveButton.textContent = 'Saving...';
-//     setTimeout(() => {
-//       location.href = 'adminEvent';
-//     }, 1000);
-//   }
-// };
+  if (!errorExist) {
+    update_array();
+    saveButton.textContent = 'Saving...';
+    setTimeout(() => {
+      location.href = 'Admin-EventPage.html';
+    }, 1000);
+  }
+});
 
 window.update_array = function () {
     var i = localStorage.getItem("updateId")
@@ -150,10 +146,10 @@ window.update_array = function () {
       imageName = imgLocalPathArr[imgLocalPathArr.length-1];
       }
     else{
-      imageName=eventArray[i].imageId;
+      imageName=dummyResponse.Event[i].imageId;
     }
-    var eventId = eventArray[i].eventId
-    var adminId = eventArray[i].adminId 
+    var eventId = dummyResponse.Event[i].eventId
+    var adminId = dummyResponse.Event[i].adminId 
     var newEvent = {
         eventId: eventId,
         adminId: adminId,
@@ -163,8 +159,8 @@ window.update_array = function () {
         imageId: imageName, 
         location: location
     }
-    eventArray.splice(i, 1, newEvent)
-    // updateDummyData(dummyResponse)
+    dummyResponse.Event.splice(i, 1, newEvent)
+    updateDummyData(dummyResponse)
     }
 cancelButton.addEventListener('click', () => {
   
