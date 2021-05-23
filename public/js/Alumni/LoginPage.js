088c8ac9139a4633a5ee62f7ed2797ca96e904f9
nic-00 <55591460+nic-00@users.noreply.github.com>
2021-05-23 23:43:36 +0800

    update

--- public/js/Alumni/LoginPage.js
+++ public/js/Alumni/LoginPage.js
@@ -311,4 +311,16 @@ function isEmpty(obj) {
 
 }
 
+function generatePassword() {
+    var length = 8,
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+      retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+  }
+
+
+  console.log(generatePassword());
 
--- src/Domain/LoginPage/forgotPassword.php
+++ src/Domain/LoginPage/forgotPassword.php
@@ -11,6 +11,10 @@ if(isset($_POST["submit"])){
     require_once '../../../libs/PHPMailer/src/Exception.php';
 
 
+    $newPassword = randomPassword();
+    echo $newPassword;
+
+
 $mail = new PHPMailer();
 
 //smtp settings
@@ -25,10 +29,18 @@ $mail->SMTPSecure = 'ssl';
 
 //email settings
 $mail->isHTML(true);
-$mail->setFrom('no-reply@alumniSystem.com');
+$mail->SetFrom('no-reply@alumniSystem.com', 'Alumni System Admin');
+$mail->AddReplyTo('no-reply@alumniSystem.com', 'Alumni System Admin');
 $mail->AddAddress($email);
 $mail->Subject = 'Change Password';
-$mail->Body = 'A test email';
+$content = str_replace(
+    array('%password%', '%to%'),
+    array($newPassword,    $email),
+    file_get_contents('ForgotPasswordEmail.html')
+);
+$mail->msgHTML(file_get_contents('./ForgotPasswordEmail.html'), __DIR__);
+$mail->msgHTML($content, dirname(__FILE__));
+$mail->AltBody = 'A test email $newPassword';
 
 // $mail->send();
 
@@ -43,4 +55,15 @@ if ($mail->send()) {
 exit(json_encode(array("status" => $status,"response" => $response)));
 
 
+}
+
+function randomPassword() {
+    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    $pass = array(); //remember to declare $pass as an array
+    $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+    for ($i = 0; $i < 8; $i++) {
+        $n = rand(0, $alphaLength);
+        $pass[] = $alphabet[$n];
+    }
+    return implode($pass); //turn the array into a string
 }
