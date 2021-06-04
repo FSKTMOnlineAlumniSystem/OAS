3bf6e4e2c31de6cb96deb2397d91958abd6de08c
nic-00 <55591460+nic-00@users.noreply.github.com>
2021-06-04 21:55:17 +0800

    update

--- src/Domain/LoginPage/LoginPage.php
+++ src/Domain/LoginPage/LoginPage.php
@@ -13,9 +13,9 @@
     <!-- BOOTSTRAP -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
         integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="/src/css\Alumni\index.css">
-    <link rel="stylesheet" href="/public/css/Alumni/EditMyProfilePage.css">
-    <link rel="stylesheet" href="/public/css/Alumni/LoginPage.css">
+    <link rel="stylesheet" type="text/css" href="../src/css\Alumni\index.css">
+    <link rel="stylesheet" href="/css/Alumni/EditMyProfilePage.css">
+    <link rel="stylesheet" href="/css/Alumni/LoginPage.css">
     
 
 <title><?= $GLOBALS['title']; ?></title>
