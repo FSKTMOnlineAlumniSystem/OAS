b8fdb09a1016dc7bfa8e5643de7cf4c134cf686e
HeLinChooi <helinchooi@gmail.com>
2021-06-09 03:27:13 +0800

    Return the event from latest as first element&add method to get first 6 events

--- src/Domain/Event/EventModel.php
+++ src/Domain/Event/EventModel.php
@@ -22,6 +22,9 @@ class EventModel
       if (!$data) {
         return array();
       }
+      // return the sorted event based on datetime
+      usort($data, fn ($a, $b) => strtotime($a['dateTime']) - strtotime($b['dateTime']));
+      $data = array_reverse($data);
       return $data;
     } catch (PDOException $exception) {
       error_log('EventModel: getAll: ' . $exception->getMessage());
@@ -50,7 +53,44 @@ class EventModel
     }
   }
   public function getEventPicture()
-    {
-        return 'data::'.$this->event['type'].';base64,'.base64_encode($this->event['imageData']);
+  {
+    return 'data::' . $this->event['type'] . ';base64,' . base64_encode($this->event['imageData']);
+  }
+  public function get6LatestEvent(): array
+  {
+    try {
+      $stmt = $this->connection->prepare('
+      SELECT * FROM events 
+      LEFT JOIN image 
+      ON events.imageId=image.imageId');
+      $stmt->execute();
+      $data = $stmt->fetchAll();
+      $this->event = $data;
+      if (!$data) {
+        return array();
+      }
+      // sort the event based on datetime
+      usort($data, fn ($a, $b) => strtotime($a['dateTime']) - strtotime($b['dateTime']));
+      $data = array_reverse(array_slice($data, -6, 6));
+      // foreach($data as $row){
+      //   echo $row['dateTime'].'<br>';
+      // }
+      return $data;
+    } catch (PDOException $exception) {
+      error_log('EventModel: get6LatestEvent: ' . $exception->getMessage());
+      throw $exception;
     }
+  }
+  private function user_compare($eventA, $eventB)
+  {
+    $dateTimeA = new DateTime($eventA['datetime']);
+    $dateTimeB = new DateTime($eventB['datetime']);
+    $interval = date_diff($dateTimeA, $dateTimeB);
+    // if ($interval === 0)
+    // return 0;
+    // else if ($x > $y)
+    // return 1;
+    // else
+    // return -1;
+  }
 }
