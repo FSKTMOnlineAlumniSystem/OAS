01cf2b77aecca8f9d78b2cecbf9fdb928dc46d7f
nic-00 <55591460+nic-00@users.noreply.github.com>
2021-06-10 18:23:03 +0800

    update

--- src/Domain/HomePage/HomePage.php
+++ src/Domain/HomePage/HomePage.php
@@ -35,17 +35,28 @@
 
 include_once '../src/Domain/Database.php';
 include_once '../src/Domain/Job/JobModel.php';
+include_once '../src/Domain/Event/EventModel.php';
 
 $db = new Database(DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD);
 
 try {
+    $event_model = new EventModel($db->getConnection());
+    $all_activities_event = $event_model->get6LatestEvent();
+    // $allImage_event = $event_model->getEventPicture();
+    // for ($i=0; $i< count($all_activities_event); $i++){
+    //     $all_activities_event[$i]['imageId'] = $allImage_event[$i];
+    // }
+    $alumni_model = new AlumniModel($db->getConnection());
+    $all_activities_alumni = $alumni_model->getAlumniListForHomePage();
+
     $job_model = new JobModel($db->getConnection());
-    $all_activities = $job_model->Nicole();
-    $allImage = $job_model->NicoleImages();
+    $all_activities_job = $job_model->Nicole();
+    $allImage_job = $job_model->NicoleImages();
         
-    for ($i=0; $i< count($all_activities); $i++){
-        $all_activities[$i]['imageId'] = $allImage[$i];
+    for ($i=0; $i< count($all_activities_job); $i++){
+        $all_activities_job[$i]['imageId'] = $allImage_job[$i];
     }
+
     // print_r($all_activities);
       
 //   $homepage_model = new HomePageModel($db->getConnection());
@@ -78,7 +89,9 @@ try {
 
 ?>
 
-<script type="text/javascript">var job_array = <?php echo json_encode($all_activities) ?>;</script>
+<script type="text/javascript">var job_array = <?php echo json_encode($all_activities_job) ?>;</script>
+<script type="text/javascript">var event_array = <?php echo json_encode($all_activities_event) ?>;</script>
+<script type="text/javascript">var alumni_array = <?php echo json_encode($all_activities_alumni) ?>;</script>
 
 <script type="module" src="/js/Alumni/homePage.js"></script>
 
