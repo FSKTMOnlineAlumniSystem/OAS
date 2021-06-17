e06312447a0480725b80587b64be7b656bdfdd2e
Yong Ming <yongming1124@gmail.com>
2021-06-17 21:48:06 +0800

    solve update password bug and remove admin-eventlocalstorage

--- src/Domain/Admin-MyProfile/AdminChangePasswordController.php
+++ src/Domain/Admin-MyProfile/AdminChangePasswordController.php
@@ -9,7 +9,7 @@ if(isset($_POST['submit']) && isset($_POST['newPassword']) && isset($_POST['oldP
         $admin = new AdminMyProfile($db->getConnection(), $_SESSION['admin']['adminId']);
         if(password_verify($_POST['oldPassword'],$admin->getPassword())){
             $admin->changePassword($_POST['newPassword']);
-            return header("Location: /adminprofile?updated=true");
+            return header("Location: /adminprofile?changepassword=success");
         }
     }catch (Exception $e) {
         // echo "Exception: " . $e->getMessage();
